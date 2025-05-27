c68d4e4d7d9e178d87b86a590e7426e4c8a7b77f
Vitaliy <ilol2021@yandex.ru>
2025-05-27 14:37:11 +0300

    Домашнее задание готово

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,15 +1,38 @@
-const holes = document.querySelectorAll('.hole');
-const deads = document.getElementById('dead');
-const losts = document.getElementById('lost');
-let countDeads = 0;
-let countlosts = 0;
+let countMoles = 0;
+let missClick = 0;
+const holes = document.getElementsByClassName('hole');
 
+function increasedCountMoles(){
+    document.getElementById('dead').textContent = ++countMoles
+}
+function increasedMissclick(){
+    document.getElementById('lost').textContent = ++missClick
+}
+function clearScore(){
+    document.getElementById('dead').textContent = 0
+    document.getElementById('lost').textContent = 0
+    countMoles = 0;
+    missClick = 0;
+}
+function winOrLose(){
+    if(countMoles === 10){
+        alert("Победа!");
+        clearScore()
+    }
+    else if(missClick === 5){
+        alert("Поражение!");
+        clearScore()
+    }
+}
 
-
-holes.forEach(hole => {
-    hole.addEventListener('click', function(){
-        this.textContent = ++countDeads
+for(let i = 0; i < holes.length; i++){
+    holes[i].addEventListener('click', function(){
+        if(holes[i].classList.contains('hole_has-mole')){
+            increasedCountMoles();
+            winOrLose()
+        }else {
+            increasedMissclick()
+            winOrLose()
+        }
     })
-})
-
-
+}
