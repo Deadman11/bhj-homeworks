2813d5a2dd5637afcdbf966525a366db6a2618b5
Vitaliy <ilol2021@yandex.ru>
2025-06-10 16:28:45 +0300

    Готово

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,10 +1,20 @@
-let tabs = document.querySelectorAll('.tab');
+let tabs = Array.from(document.querySelectorAll('.tab'));
 const tabsContent = document.querySelectorAll('.tab__content')
-const arr = [];
 
-tabs.forEach(el => el.addEventListener('click', function(e){
-    e.target
-    let clickTarget = e.target;
-    
-}))
+function chengeActiveTabs(){
+    for(const tab of tabs){
+        tab.classList = "tab"
+    }
+    for (const tabContent of tabsContent) {
+        tabContent.classList = "tab__content"
+    }
 
+    let i = tabs.indexOf(this);
+
+    tabs[i].classList = "tab tab_active";
+    tabsContent[i].classList = "tab__content tab__content_active"
+}
+
+for(const tbs of tabs){
+    tbs.addEventListener('click', chengeActiveTabs);
+}
